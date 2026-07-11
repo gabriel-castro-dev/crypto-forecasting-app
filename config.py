1340ed001d3d0022b8b1fed125e336050b3d9601
Gabriel Castro <gabriel.inacio.1622289@sga.pucminas.br>
2026-07-11 12:41:59 -0300

    atualizando config.py para classe pydantic

--- config.py
+++ config.py
@@ -1,33 +1,35 @@
-# ============================================================================
-# CONFIGURAÇÕES GLOBAIS DO PROJETO
-# ============================================================================
-
 import logging
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-# Variáveis de Retry
-MAX_RETRIES = 3
-RETRY_DELAY = 5  # segundos
+class Settings(BaseSettings):
+    MAX_RETRIES = 3
+    RETRY_DELAY = 5  
+    SUPABASE_URL: str
+    SUPABASE_KEY: str
+    BINANCE_API_KEY: str
+    BINANCE_API_SECRET: str
+    USE_TESTNET = True
 
-# Testnet vs Mainnet
-USE_TESTNET = True
+    LOG_LEVEL = logging.INFO
+    LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    LOG_DATE_FORMAT = '%d/%m/%Y %H:%M:%S'
 
-# ============================================================================
-# CONFIGURAÇÃO DE LOGGING
-# ============================================================================
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-LOG_LEVEL = logging.INFO
-LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-LOG_DATE_FORMAT = '%d/%m/%Y %H:%M:%S'
 
-def setup_logging():
-    """Configura o logging estruturado para toda a aplicação."""
+def setup_logging():  
+        
     logging.basicConfig(
-        level=LOG_LEVEL,
-        format=LOG_FORMAT,
-        datefmt=LOG_DATE_FORMAT,
+        level=settings.LOG_LEVEL,         
+        format=settings.LOG_FORMAT,       
+        datefmt=settings.LOG_DATE_FORMAT,
         handlers=[
-            logging.StreamHandler(),  # Console output
-            logging.FileHandler('app.log', mode='a')  # File output
-        ]
-    )
+            logging.StreamHandler(),
+            logging.FileHandler('app.log', mode='a')
+            ]
+        )
+
+settings = Settings()
+
+
 
